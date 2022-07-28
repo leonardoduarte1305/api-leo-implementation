ed8f9d081722f61f6864fdd74740b67e99445e27
Leonardo Duarte <leonardoduarte1305@gmail.com>
2022-07-28 15:48:38 -0300

    Created the app-test module. Updated some configs.

---
+++ app-test/src/test/java/dev/leoduarte/api/leo/implementation/apptest/AppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.api.leo.implementation.apptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
