5939e5f692b8460c8ba5a23337ed1c87c25c76f0
Leonardo Duarte <codebrain.leonardo.dasilva@kiwigrid.com>
2022-09-13 04:59:44 -0300

    Updated Flyway to consider the last db migration.

---
+++ app/src/main/resources/db/migration/V2__populate_tables.sql
@@ -0,0 +1,13 @@
+INSERT INTO profile (profile_id, role) VALUES (null, 'MANAGER');
+INSERT INTO profile (profile_id, role) VALUES (null, 'ADMIN');
+INSERT INTO profile (profile_id, role) VALUES (null, 'SUPERVISOR');
+INSERT INTO profile (profile_id, role) VALUES (null, 'USER');
+
+INSERT INTO department (department_id, description) VALUES (null, 'RH');
+INSERT INTO department (department_id, description) VALUES (null, 'Payment');
+INSERT INTO department (department_id, description) VALUES (null, 'Charge');
+INSERT INTO department (department_id, description) VALUES (null, 'Management');
+INSERT INTO department (department_id, description) VALUES (null, 'Board of Directors');
+INSERT INTO department (department_id, description) VALUES (null, 'Development');
+INSERT INTO department (department_id, description) VALUES (null, 'Investor Relationship');
+INSERT INTO department (department_id, description) VALUES (null, 'Logistic Support');
